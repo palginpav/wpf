a3177c26f0f519442d9027aaa0a97436ada8ea36
Pavel Palgin <palginpav@gmail.com>
2026-03-26 19:00:02 +0300

    WindowsBase: implement MediaPermission and WebBrowserPermission

--- src/Microsoft.DotNet.Wpf/src/WindowsBase/System/Security/Permissions/MediaPermission.cs
+++ src/Microsoft.DotNet.Wpf/src/WindowsBase/System/Security/Permissions/MediaPermission.cs
@@ -5,10 +5,10 @@
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -30,81 +30,212 @@ namespace System.Security.Permissions {
 
 	public sealed class MediaPermission : CodeAccessPermission, IUnrestrictedPermission
 	{
+		private MediaPermissionAudio _mediaPermissionAudio;
+		private MediaPermissionVideo _mediaPermissionVideo;
+		private MediaPermissionImage _mediaPermissionImage;
+
 		public MediaPermission ()
 		{
-			throw new NotImplementedException ();
+			InitDefaults ();
 		}
 
 		public MediaPermission (MediaPermissionAudio permissionAudio)
 		{
-			throw new NotImplementedException ();
+			VerifyMediaPermissionAudio (permissionAudio);
+			InitDefaults ();
+			_mediaPermissionAudio = permissionAudio;
 		}
 
 		public MediaPermission (MediaPermissionImage permissionImage)
 		{
-			throw new NotImplementedException ();
+			VerifyMediaPermissionImage (permissionImage);
+			InitDefaults ();
+			_mediaPermissionImage = permissionImage;
 		}
 
 		public MediaPermission (MediaPermissionVideo permissionVideo)
 		{
-			throw new NotImplementedException ();
+			VerifyMediaPermissionVideo (permissionVideo);
+			InitDefaults ();
+			_mediaPermissionVideo = permissionVideo;
 		}
 
 		public MediaPermission (PermissionState state)
 		{
-			throw new NotImplementedException ();
+			if (state == PermissionState.Unrestricted) {
+				_mediaPermissionAudio = MediaPermissionAudio.AllAudio;
+				_mediaPermissionVideo = MediaPermissionVideo.AllVideo;
+				_mediaPermissionImage = MediaPermissionImage.AllImage;
+			} else if (state == PermissionState.None) {
+				_mediaPermissionAudio = MediaPermissionAudio.NoAudio;
+				_mediaPermissionVideo = MediaPermissionVideo.NoVideo;
+				_mediaPermissionImage = MediaPermissionImage.NoImage;
+			} else {
+				throw new ArgumentException ("InvalidPermissionState");
+			}
 		}
 
 		public MediaPermission (MediaPermissionAudio permissionAudio, MediaPermissionVideo permissionVideo, MediaPermissionImage permissionImage)
 		{
-			throw new NotImplementedException ();
+			VerifyMediaPermissionAudio (permissionAudio);
+			VerifyMediaPermissionVideo (permissionVideo);
+			VerifyMediaPermissionImage (permissionImage);
+			_mediaPermissionAudio = permissionAudio;
+			_mediaPermissionVideo = permissionVideo;
+			_mediaPermissionImage = permissionImage;
 		}
 
 		public MediaPermissionAudio Audio {
-			get { throw new NotImplementedException (); }
+			get { return _mediaPermissionAudio; }
 		}
 
 		public MediaPermissionVideo Video {
-			get { throw new NotImplementedException (); }
+			get { return _mediaPermissionVideo; }
 		}
 
 		public MediaPermissionImage Image {
-			get { throw new NotImplementedException (); }
+			get { return _mediaPermissionImage; }
 		}
 
 		public override IPermission Copy ()
 		{
-			throw new NotImplementedException ();
+			return new MediaPermission (_mediaPermissionAudio, _mediaPermissionVideo, _mediaPermissionImage);
 		}
 
 		public override void FromXml (SecurityElement securityElement)
 		{
-			throw new NotImplementedException ();
+			if (securityElement == null)
+				throw new ArgumentNullException ("securityElement");
+
+			string classAttr = securityElement.Attribute ("class");
+			if (classAttr == null || classAttr.IndexOf (GetType ().FullName, StringComparison.Ordinal) == -1)
+				throw new ArgumentNullException ("securityElement");
+
+			string unrestricted = securityElement.Attribute ("Unrestricted");
+			if (unrestricted != null && bool.TrueString.Equals (unrestricted, StringComparison.OrdinalIgnoreCase)) {
+				_mediaPermissionAudio = MediaPermissionAudio.AllAudio;
+				_mediaPermissionVideo = MediaPermissionVideo.AllVideo;
+				_mediaPermissionImage = MediaPermissionImage.AllImage;
+				return;
+			}
+
+			InitDefaults ();
+
+			string audio = securityElement.Attribute ("Audio");
+			if (audio != null)
+				_mediaPermissionAudio = (MediaPermissionAudio) Enum.Parse (typeof (MediaPermissionAudio), audio);
+			else
+				throw new ArgumentException ("BadXml: audio");
+
+			string video = securityElement.Attribute ("Video");
+			if (video != null)
+				_mediaPermissionVideo = (MediaPermissionVideo) Enum.Parse (typeof (MediaPermissionVideo), video);
+			else
+				throw new ArgumentException ("BadXml: video");
+
+			string image = securityElement.Attribute ("Image");
+			if (image != null)
+				_mediaPermissionImage = (MediaPermissionImage) Enum.Parse (typeof (MediaPermissionImage), image);
+			else
+				throw new ArgumentException ("BadXml: image");
 		}
 
 		public override IPermission Intersect (IPermission target)
 		{
-			throw new NotImplementedException ();
+			if (target == null)
+				return null;
+
+			MediaPermission other = target as MediaPermission;
+			if (other == null)
+				throw new ArgumentException ("TargetNotMediaPermissionLevel");
+
+			MediaPermissionAudio audio = _mediaPermissionAudio < other._mediaPermissionAudio ? _mediaPermissionAudio : other._mediaPermissionAudio;
+			MediaPermissionVideo video = _mediaPermissionVideo < other._mediaPermissionVideo ? _mediaPermissionVideo : other._mediaPermissionVideo;
+			MediaPermissionImage image = _mediaPermissionImage < other._mediaPermissionImage ? _mediaPermissionImage : other._mediaPermissionImage;
+
+			return new MediaPermission (audio, video, image);
 		}
 
 		public override bool IsSubsetOf (IPermission target)
 		{
-			throw new NotImplementedException ();
+			if (target == null)
+				return EqualsLevel (MediaPermissionAudio.NoAudio, MediaPermissionVideo.NoVideo, MediaPermissionImage.NoImage);
+
+			MediaPermission other = target as MediaPermission;
+			if (other == null)
+				throw new ArgumentException ("TargetNotMediaPermissionLevel");
+
+			return _mediaPermissionAudio <= other._mediaPermissionAudio &&
+			       _mediaPermissionVideo <= other._mediaPermissionVideo &&
+			       _mediaPermissionImage <= other._mediaPermissionImage;
 		}
 
 		public bool IsUnrestricted ()
 		{
-			throw new NotImplementedException ();
+			return EqualsLevel (MediaPermissionAudio.AllAudio, MediaPermissionVideo.AllVideo, MediaPermissionImage.AllImage);
 		}
 
 		public override SecurityElement ToXml ()
 		{
-			throw new NotImplementedException ();
+			SecurityElement elem = new SecurityElement ("IPermission");
+			elem.AddAttribute ("class", GetType ().AssemblyQualifiedName);
+			elem.AddAttribute ("version", "1");
+			if (IsUnrestricted ()) {
+				elem.AddAttribute ("Unrestricted", bool.TrueString);
+			} else {
+				elem.AddAttribute ("Audio", _mediaPermissionAudio.ToString ());
+				elem.AddAttribute ("Video", _mediaPermissionVideo.ToString ());
+				elem.AddAttribute ("Image", _mediaPermissionImage.ToString ());
+			}
+			return elem;
 		}
 
 		public override IPermission Union (IPermission target)
 		{
-			throw new NotImplementedException ();
+			if (target == null)
+				return Copy ();
+
+			MediaPermission other = target as MediaPermission;
+			if (other == null)
+				throw new ArgumentException ("TargetNotMediaPermissionLevel");
+
+			MediaPermissionAudio audio = _mediaPermissionAudio > other._mediaPermissionAudio ? _mediaPermissionAudio : other._mediaPermissionAudio;
+			MediaPermissionVideo video = _mediaPermissionVideo > other._mediaPermissionVideo ? _mediaPermissionVideo : other._mediaPermissionVideo;
+			MediaPermissionImage image = _mediaPermissionImage > other._mediaPermissionImage ? _mediaPermissionImage : other._mediaPermissionImage;
+
+			return new MediaPermission (audio, video, image);
+		}
+
+		internal static void VerifyMediaPermissionAudio (MediaPermissionAudio level)
+		{
+			if (level < MediaPermissionAudio.NoAudio || level > MediaPermissionAudio.AllAudio)
+				throw new ArgumentException ("InvalidPermissionLevel");
+		}
+
+		internal static void VerifyMediaPermissionVideo (MediaPermissionVideo level)
+		{
+			if (level < MediaPermissionVideo.NoVideo || level > MediaPermissionVideo.AllVideo)
+				throw new ArgumentException ("InvalidPermissionLevel");
+		}
+
+		internal static void VerifyMediaPermissionImage (MediaPermissionImage level)
+		{
+			if (level < MediaPermissionImage.NoImage || level > MediaPermissionImage.AllImage)
+				throw new ArgumentException ("InvalidPermissionLevel");
+		}
+
+		private void InitDefaults ()
+		{
+			_mediaPermissionAudio = MediaPermissionAudio.SafeAudio;
+			_mediaPermissionVideo = MediaPermissionVideo.SafeVideo;
+			_mediaPermissionImage = MediaPermissionImage.SafeImage;
+		}
+
+		private bool EqualsLevel (MediaPermissionAudio audioLevel, MediaPermissionVideo videoLevel, MediaPermissionImage imageLevel)
+		{
+			return _mediaPermissionAudio == audioLevel &&
+			       _mediaPermissionVideo == videoLevel &&
+			       _mediaPermissionImage == imageLevel;
 		}
 	}
 
--- src/Microsoft.DotNet.Wpf/src/WindowsBase/System/Security/Permissions/WebBrowserPermission.cs
+++ src/Microsoft.DotNet.Wpf/src/WindowsBase/System/Security/Permissions/WebBrowserPermission.cs
@@ -5,10 +5,10 @@
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -30,61 +30,124 @@ namespace System.Security.Permissions {
 
 	public sealed class WebBrowserPermission : CodeAccessPermission, IUnrestrictedPermission
 	{
+		private WebBrowserPermissionLevel _webBrowserPermissionLevel;
+
 		public WebBrowserPermission ()
 		{
-			throw new NotImplementedException ();
+			_webBrowserPermissionLevel = WebBrowserPermissionLevel.Safe;
 		}
 
 		public WebBrowserPermission (PermissionState state)
 		{
-			throw new NotImplementedException ();
+			if (state == PermissionState.Unrestricted)
+				_webBrowserPermissionLevel = WebBrowserPermissionLevel.Unrestricted;
+			else if (state == PermissionState.None)
+				_webBrowserPermissionLevel = WebBrowserPermissionLevel.None;
+			else
+				throw new ArgumentException ("InvalidPermissionState");
 		}
 
 		public WebBrowserPermission (WebBrowserPermissionLevel webBrowserPermissionLevel)
 		{
-			throw new NotImplementedException ();
+			VerifyWebBrowserPermissionLevel (webBrowserPermissionLevel);
+			_webBrowserPermissionLevel = webBrowserPermissionLevel;
 		}
 
 		public WebBrowserPermissionLevel Level {
-			get { throw new NotImplementedException (); }
-			set { throw new NotImplementedException (); }
+			get { return _webBrowserPermissionLevel; }
+			set {
+				VerifyWebBrowserPermissionLevel (value);
+				_webBrowserPermissionLevel = value;
+			}
 		}
 
 		public override IPermission Copy ()
 		{
-			throw new NotImplementedException ();
+			return new WebBrowserPermission (_webBrowserPermissionLevel);
 		}
 
 		public override void FromXml (SecurityElement securityElement)
 		{
-			throw new NotImplementedException ();
+			if (securityElement == null)
+				throw new ArgumentNullException ("securityElement");
+
+			string classAttr = securityElement.Attribute ("class");
+			if (classAttr == null || classAttr.IndexOf (GetType ().FullName, StringComparison.Ordinal) == -1)
+				throw new ArgumentNullException ("securityElement");
+
+			string unrestricted = securityElement.Attribute ("Unrestricted");
+			if (unrestricted != null && bool.TrueString.Equals (unrestricted, StringComparison.OrdinalIgnoreCase)) {
+				_webBrowserPermissionLevel = WebBrowserPermissionLevel.Unrestricted;
+				return;
+			}
+
+			string level = securityElement.Attribute ("Level");
+			if (level != null)
+				_webBrowserPermissionLevel = (WebBrowserPermissionLevel) Enum.Parse (typeof (WebBrowserPermissionLevel), level);
+			else
+				_webBrowserPermissionLevel = WebBrowserPermissionLevel.None;
 		}
 
 		public override IPermission Intersect (IPermission target)
 		{
-			throw new NotImplementedException ();
+			if (target == null)
+				return null;
+
+			WebBrowserPermission other = target as WebBrowserPermission;
+			if (other == null)
+				throw new ArgumentException ("TargetNotWebBrowserPermissionLevel");
+
+			WebBrowserPermissionLevel level = _webBrowserPermissionLevel < other._webBrowserPermissionLevel ? _webBrowserPermissionLevel : other._webBrowserPermissionLevel;
+			return new WebBrowserPermission (level);
 		}
 
 		public override bool IsSubsetOf (IPermission target)
 		{
-			throw new NotImplementedException ();
+			if (target == null)
+				return _webBrowserPermissionLevel == WebBrowserPermissionLevel.None;
+
+			WebBrowserPermission other = target as WebBrowserPermission;
+			if (other == null)
+				throw new ArgumentException ("TargetNotWebBrowserPermissionLevel");
+
+			return _webBrowserPermissionLevel <= other._webBrowserPermissionLevel;
 		}
 
 		public bool IsUnrestricted ()
 		{
-			throw new NotImplementedException ();
+			return _webBrowserPermissionLevel == WebBrowserPermissionLevel.Unrestricted;
 		}
 
 		public override SecurityElement ToXml ()
 		{
-			throw new NotImplementedException ();
+			SecurityElement elem = new SecurityElement ("IPermission");
+			elem.AddAttribute ("class", GetType ().AssemblyQualifiedName);
+			elem.AddAttribute ("version", "1");
+			if (IsUnrestricted ())
+				elem.AddAttribute ("Unrestricted", bool.TrueString);
+			else
+				elem.AddAttribute ("Level", _webBrowserPermissionLevel.ToString ());
+			return elem;
 		}
 
 		public override IPermission Union (IPermission target)
 		{
-			throw new NotImplementedException ();
+			if (target == null)
+				return Copy ();
+
+			WebBrowserPermission other = target as WebBrowserPermission;
+			if (other == null)
+				throw new ArgumentException ("TargetNotWebBrowserPermissionLevel");
+
+			WebBrowserPermissionLevel level = _webBrowserPermissionLevel > other._webBrowserPermissionLevel ? _webBrowserPermissionLevel : other._webBrowserPermissionLevel;
+			return new WebBrowserPermission (level);
 		}
 
+		internal static void VerifyWebBrowserPermissionLevel (WebBrowserPermissionLevel level)
+		{
+			if (level < WebBrowserPermissionLevel.None || level > WebBrowserPermissionLevel.Unrestricted)
+				throw new ArgumentException ("InvalidPermissionLevel");
+		}
 	}
 
 }
